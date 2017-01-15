ce778a0d830469fe0e2cb93dfdd1c87a9b34542b
mithrandil007 <vivek.h57@gmail.com>
2017-01-14 21:03:00 -0500

    Add files via upload

---
+++ Ch5/q2.c
@@ -0,0 +1,33 @@
+// C_Primer_Solutions_Ch5.cpp : Defines the entry point for the console application.
+//
+
+//Chapter 5 exercise 1
+/*#include<stdio.h>
+#define MPH 60
+int main(void) {
+	int min;
+	int hours, minutes;
+	printf("Enter time in minutes\n");
+	scanf("%d", &min);
+	while (min > 0) {
+		hours = min / MPH;
+		minutes = min%MPH;
+		printf("%d minutes is equivalent to %d hours %d minutes\n", min, hours, minutes);
+		printf("Enter time in minutes\n");
+		scanf("%d", &min);
+	}
+	return 0;
+}*/
+#include<stdio.h>
+int main(void)
+{
+	int num, i = 0;
+	printf("Enter the number\n");
+	scanf("%d", &num);
+	while (i <= 10) {
+		printf("%d \n", num + i);
+		i++;
+	}
+	return 0;
+}
+
