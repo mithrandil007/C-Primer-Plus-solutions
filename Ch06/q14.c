353422dffe6dd9c57dbf4b4f3539232370528e29
mithrandil007 <vivek.h57@gmail.com>
2017-01-29 18:47:26 -0500

    Add files via upload

---
+++ Ch06/q14.c
@@ -0,0 +1,67 @@
+/*#include<stdio.h>
+
+double Temperatures(double temp);
+int main(void) {
+	double temp;
+	int num;
+	char t;
+	while (scanf("%d", &num) == 1);
+	printf("Please enter temperature to be converted");
+	scanf("%c", &t);
+	while (scanf("%lf", &temp)) {
+		Temperatures(temp);
+		printf("Please enter temperature to be converted");
+	}
+
+	return 0;
+
+}
+double Temperatures(double temp) {
+	const double CM = (double) 5/9;
+	const double CA = -32.0;
+	const double KA = 273.16;
+	double kelvin, celcius;
+	celcius = (temp + CA)*CM;
+	kelvin = celcius + KA;
+	printf("Temperature in celcius:%.2lf \nTemperature in kelvin: %.2lf\n", celcius, kelvin);
+}*/
+
+/*#include<stdio.h>
+#define SIZE 10
+#define PAR 72
+int main(void) {
+	int index, score[SIZE];
+	int sum = 0;
+	float average;
+	printf("Enter %d golf scores:\n", SIZE);
+	for (index = 0; index < SIZE; index++)
+		scanf("%d", &score[index]);
+	printf("The scores read in are as follows:\n");
+	for (index = 0; index < SIZE; index++)
+		printf("%5d", score[index]);
+	printf("\n");
+	for (index = 0; index < SIZE; index++)
+		sum += score[index];
+	average = (float)sum / SIZE;
+	printf("Sum = %d, average = %.2f\n", sum, average);
+	printf("That's a handicap of %.0f.\n", average - PAR);
+
+	return 0;
+}*/
+
+#include<stdio.h>
+int main(void)
+{
+	char string[255];
+	int i = 0; 
+	printf("Enter a string:");
+	while (scanf("%c", &string[i]), string[i] != '\n') {
+		i++;
+	}
+	for (; i >= 0; i--) 
+		printf("%c", string[i]);
+
+	printf("\n");
+
+	return 0;
+}
